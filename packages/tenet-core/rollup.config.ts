8e38149a21d1d0c0a2a533bed4453ca8d6091cef
Yukee-798 <yukee798@gmail.com>
2023-03-05 20:02:54 +0800

    chore: add rollup.config.ts to tenet-core

--- packages/tenet-core/rollup.config.ts
+++ packages/tenet-core/rollup.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'rollup';
 import typescript from '@rollup/plugin-typescript';
 import babel from '@rollup/plugin-babel';
 import del from 'rollup-plugin-delete';
+import type { RollupOptions } from 'rollup';
 
-export default defineConfig({
+const config: RollupOptions = {
   input: './index.ts',
   output: {
     file: 'dist/index.js',
     format: 'esm', // 使用 esm 打包输出，便于 vite 识别
   },
   plugins: [
-    del({ targets: 'dist/*' }), // 删除 dist 目录下的所有文件
+    // del({ targets: 'dist/*' }), // 删除 dist 目录下的所有文件
     typescript({
       tsconfig: './tsconfig.json',
       sourceMap: true,
@@ -21,5 +21,10 @@ export default defineConfig({
       presets: ['@babel/preset-env'],
     }),
   ],
+  watch: {
+    include: ['**/*.ts'],
+  },
   external: ['react'],
-});
+};
+
+export default config;
