7753c1ef1fbb736e171de59e8e350fa2e7c1d949
Yukee-798 <yukee798@gmail.com>
2023-03-07 16:02:15 +0800

    chore: update root eslint config & add root eslint config

--- .eslintrc.react.cjs
+++ .eslintrc.react.cjs
@@ -30,6 +30,8 @@ module.exports = {
       },
     ],
     '@typescript-eslint/ban-ts-comment': 'off',
-    "@typescript-eslint/no-empty-interface": "off",
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
   },
 };
